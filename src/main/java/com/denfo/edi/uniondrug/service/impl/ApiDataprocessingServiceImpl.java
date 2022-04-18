e56de16add92f0d2a32dec44fa06bfac17d5596d
万清刚 <13396091869@163.com>
2022-04-18 17:44:26 +0800

    修复页数判断

--- src/main/java/com/denfo/edi/uniondrug/service/impl/ApiDataprocessingServiceImpl.java
+++ src/main/java/com/denfo/edi/uniondrug/service/impl/ApiDataprocessingServiceImpl.java
@@ -36,13 +36,13 @@ public class ApiDataprocessingServiceImpl implements ApiDataprocessingService {
             return "Warning: " + method + "接口无请求参数！";
         }
         JSONObject requestJson = JSON.parseObject(interfaceLog.getRequest());
-        System.out.println(requestJson);
         if (requestJson.isEmpty()) {
             return "Warning: " + method + "接口无请求！";
         }
 
         while (flag) {
             try {
+                System.out.println(requestJson);
                 String sign = Sign.getSign(requestJson);
                 Date date = new Date();
                 SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -51,34 +51,35 @@ public class ApiDataprocessingServiceImpl implements ApiDataprocessingService {
                 // 测试使用固定sign
 //                sign = testSign;
                 requestJson.put("sign", sign);
-                System.out.println(sign);
+                // System.out.println(sign);
                 String result = HttpPostUtil.getPostResult(httpUrl, requestJson.toString());
                 // System.out.println(result);
                 JSONObject jsonObject = JSONObject.parseObject(result);
-//                System.out.println(jsonObject.toString());
+               System.out.println(jsonObject.toString());
                  interfaceLog.setResponse(result);
                 interfaceLog.setStatus(1);
                 if (method == "trackingStatus" || requestJson.getInteger("num") == 1) {
                     interfaceLogDao.updateLog(interfaceLog);
                 }
                 else {
-                    requestJson.remove("time");
-                    requestJson.remove("sign");
                     interfaceLog.setRequest(requestJson.toString());
                     interfaceLogDao.insertLog(interfaceLog);
                 }
                 data = jsonObject.getString("data");
                 // System.out.println(data);
                 Integer count = jsonObject.getInteger("count");
-                System.out.println(count);
+                Integer size = requestJson.getInteger("size");
+                System.out.println("count = " + count);
 
                 if (count != null) {
-                    if (count <= 100 || num >= (count/100.0)) {
+                    if (count <= size || num >= (count/(size * 1.0))) {
                         flag = false;
                     }
                     else {
                         num++;
-                        requestJson.replace("num", num);
+                        requestJson.replace("num", num.toString());
+                        requestJson.remove("time");
+                        requestJson.remove("sign");
 
                     }
                 }
